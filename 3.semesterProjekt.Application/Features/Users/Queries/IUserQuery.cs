239816b3cd1935056d7527f3299f834562b3ef3e
Emeli Isabel Egholm Kissov <eiek52989@edu.ucl.dk>
2024-05-13 14:40:26 +0200

    Application features added, plus interfaces in user.

---
+++ 3.semesterProjekt.Application/Features/Users/Queries/IUserQuery.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3.semesterProjekt.Application.Features.Users.Queries
+{
+    public interface IUserQuery
+    {
+        Task<IEnumerable<UserDto>> GetUserByUnionId(Guid unionId);
+        Task<UserDto> GetUserById(Guid userId);
+
+    }
+}
