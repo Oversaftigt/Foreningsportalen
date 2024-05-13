281c92ac045d7503fd585e3da102ef57072f8837
Oversaftigt <32249246+Oversaftigt@users.noreply.github.com>
2024-05-13 23:53:49 +0200

    Masser af klasser og interfaces inde i features/address

---
+++ 3.semesterProjekt.Application/Features/Addresses/Commands/Implementations/AddressCommand.cs
@@ -0,0 +1,28 @@
+﻿using _3.semesterProjekt.Application.Features.Addresses.Commands.DTOs;
+using _3.semesterProjekt.Application.Features.Addresses.Commands.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3.semesterProjekt.Application.Features.Addresses.Commands.Implementations
+{
+    public class AddressCommand : IAddressCommand
+    {
+        Task IAddressCommand.CreateAddress(AddressCreateRequestDto addressCreateRequestDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task IAddressCommand.UpdateAddress(AddressUpdateRequestDto addressUpdateRequestDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task IAddressCommand.DeleteAddress(Guid addressId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
