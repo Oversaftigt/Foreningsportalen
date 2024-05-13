356aa2fffa4b3047d1fe52aaa250e1009e95ecfd
SANNESPC\Sanne <sharrekilde@gmail.com>
2024-05-13 09:53:50 +0200

    deleted unessesary usings

--- 3.semesterProjekt.Domain/Entities/User.cs
+++ 3.semesterProjekt.Domain/Entities/User.cs
@@ -1,11 +1,4 @@
-﻿using _3.semesterProjekt.Domain.Values;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace _3.semesterProjekt.Domain.Entities
+﻿namespace _3.semesterProjekt.Domain.Entities
 {
     public class User
     {
