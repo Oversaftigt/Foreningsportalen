24ee2163a29e46ae6ae73b4309e73f95c02f800a
Martin Ngo <mng@unik.dk>
2024-05-13 13:08:36 +0200

    Bookingcontroller - api start

--- 3.semesterProjekt.Api/Controllers/BookingController.cs
+++ 3.semesterProjekt.Api/Controllers/BookingController.cs
@@ -1,12 +1,38 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using _3.semesterProjekt.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
 
 namespace _3.semesterProjekt.Api.Controllers
 {
     public class BookingController
     {
         [HttpGet]
-        
-    
+        public ActionResult<Booking> GetBooking()
+        {
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Booking>> GetAllBookings()
+        {
+
+        }
+
+        [HttpPut]
+        public IActionResult UpdateBooking()
+        {
+
+        }
+
+        [HttpPost]
+        public IActionResult PostBooking()
+        {
+
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteBooking()
+        {
+
+        }
         
     }
 }
