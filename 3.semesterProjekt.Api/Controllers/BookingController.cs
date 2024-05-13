530bd6f00cb2473ecbddd7808ddd511a556f672f
Martin Ngo <mng@unik.dk>
2024-05-13 11:14:34 +0200

    Live share apitest

--- 3.semesterProjekt.Api/Controllers/BookingController.cs
+++ 3.semesterProjekt.Api/Controllers/BookingController.cs
@@ -1,6 +1,12 @@
-﻿namespace _3.semesterProjekt.Api.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _3.semesterProjekt.Api.Controllers
 {
     public class BookingController
     {
+        [HttpGet]
+        
+    
+        
     }
 }
--- 3.semesterProjekt.Domain/Entities/User.cs
+++ 3.semesterProjekt.Domain/Entities/User.cs
@@ -1,4 +1,10 @@
-﻿namespace _3.semesterProjekt.Domain.Entities
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3.semesterProjekt.Domain.Entities
 {
     public class User
     {
