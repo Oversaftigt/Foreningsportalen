3eacd656f8200d1830af89de08c8edeeafc285e9
Emeli Isabel Egholm Kissov <eiek52989@edu.ucl.dk>
2024-05-14 12:31:18 +0200

    Updated User and UnionMember properties

--- 3.semesterProjekt.Domain/Entities/User.cs
+++ 3.semesterProjekt.Domain/Entities/User.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using _3.semesterProjekt.Domain.Shared;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,11 +7,8 @@ using System.Threading.Tasks;
 
 namespace _3.semesterProjekt.Domain.Entities
 {
-    public class User
+    public class User : Entity
     {
-        public int Id { get; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
 
