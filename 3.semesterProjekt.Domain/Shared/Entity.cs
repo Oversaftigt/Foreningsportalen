38f9fb2c16fea3b86917d4e374b183497cdcf7be
SANNESPC\Sanne <sharrekilde@gmail.com>
2024-05-14 09:26:07 +0200

    entity class i shared domain skrevet

--- 3.semesterProjekt.Domain/Shared/Entity.cs
+++ 3.semesterProjekt.Domain/Shared/Entity.cs
@@ -1,6 +1,10 @@
-﻿namespace _3.semesterProjekt.Domain.Shared;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace _3.semesterProjekt.Domain.Shared;
 
 public class Entity
 {
-
+    [Timestamp]
+    public DateTime RowVersion { get; set; }
+    public Guid Id { get; set; }
 }
