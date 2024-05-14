e7ee0b91c382d650c9f54e7fe2fc950224e0d6e8
SANNESPC\Sanne <sharrekilde@gmail.com>
2024-05-14 12:43:03 +0200

    Tilføjet Guid id plus ctor til user

--- 3.semesterProjekt.Domain/Entities/User.cs
+++ 3.semesterProjekt.Domain/Entities/User.cs
@@ -1,6 +1,7 @@
 ﻿using _3.semesterProjekt.Domain.Shared;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,18 @@ namespace _3.semesterProjekt.Domain.Entities
 {
     public class User : Entity
     {
+        public User() : base(Guid.NewGuid())
+        {
+            
+        }
+
+        public User(Guid id, string email, string phoneNumber) : base(id)
+        {
+            this.Id = id;
+            Email = email;
+            PhoneNumber = phoneNumber;
+        }
+
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
 
