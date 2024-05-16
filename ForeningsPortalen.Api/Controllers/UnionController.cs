927ac93788663e10940d9ac6b552d200326c6244
SANNESPC\Sanne <sharrekilde@gmail.com>
2024-05-16 12:02:05 +0200

    read methods in union controller

--- ForeningsPortalen.Api/Controllers/UnionController.cs
+++ ForeningsPortalen.Api/Controllers/UnionController.cs
@@ -1,36 +1,54 @@
-﻿using ForeningsPortalen.Domain.Entities;
+﻿using ForeningsPortalen.Application.Features.Unions.Commands;
+using ForeningsPortalen.Application.Features.Unions.Commands.DTOs;
+using ForeningsPortalen.Application.Features.Unions.Queries;
+using ForeningsPortalen.Domain.Entities;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace ForeningsPortalen.Api.Controllers
 {
-    public class UnionController
+    public class UnionController : ControllerBase
     {
-        [HttpGet]
-        public ActionResult<Union> GetUnion()
+        private readonly IUnionQuery _Query;
+        public UnionController(IUnionQuery query, IUnionCommands commands)
         {
-            throw new NotImplementedException();
+            _Query = query;
+        }
+        [HttpGet]
+        public ActionResult<Union> GetUnion(Guid id)
+        {
+            var result = _Query.GetUnionWithId(id);
+            if(result == null)
+                return NotFound(result);
+           
+            else return Ok(result);
         }
 
         [HttpGet]
         public ActionResult<IEnumerable<Union>> GetAllUnions()
         {
-            throw new NotImplementedException();
+            var result = _Query.GetAllUnions().ToList();
+            if(!result.Any())
+                return NotFound(result);
+
+           else return Ok(result);
         }
 
         [HttpPut]
-        public ActionResult UpdateUnion()
+        public ActionResult UpdateUnion(UnionCommandUpdateDto unionCommandUpdateDto)
         {
             throw new NotImplementedException();
         }
 
         [HttpPost]
-        public ActionResult PostUnion()
+        public ActionResult CreateUnion(UnionCommandCreateDto unionCommandCreateDto)
         {
             throw new NotImplementedException();
         }
 
         [HttpDelete]
-        public ActionResult DeleteUnion()
+        public ActionResult DeleteUnion(Guid id)
         {
             throw new NotImplementedException();
         }
