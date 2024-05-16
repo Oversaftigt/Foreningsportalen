c184be420c1ee25893d2f118427fc22baa023ffb
SANNESPC\Sanne <sharrekilde@gmail.com>
2024-05-16 14:06:01 +0200

    infrastructure union implemented

---
+++ ForeningsPortalen.Infrastructure/Queries/UnionQueries.cs
@@ -0,0 +1,23 @@
+﻿using ForeningsPortalen.Application.Features.Unions.Queries;
+using ForeningsPortalen.Application.Features.Unions.Queries.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForeningsPortalen.Infrastructure.Queries
+{
+    public class UnionQueries : IUnionQueries
+    {
+        UnionQueryResultDto IUnionQueries.GelAllUnions()
+        {
+            throw new NotImplementedException();
+        }
+
+        UnionQueryResultDto IUnionQueries.GelOneUnionById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
