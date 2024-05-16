059663d67071ef9fc6f9d9d5cb02fb8eaa271ecb
Emeli Isabel Egholm Kissov <eiek52989@edu.ucl.dk>
2024-05-16 10:04:16 +0200

    Changes CleanUp files

--- ForeningPortalen.WebApp/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ForeningPortalen.WebApp.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
