687df11d7725c2cbb7ffb6d7cd91045209ed4f00
SANNESPC\Sanne <sharrekilde@gmail.com>
2024-05-17 20:21:54 +0200

    small changes

--- ForeningsPortalen.Infrastructure/Queries/UnionQueries.cs
+++ ForeningsPortalen.Infrastructure/Queries/UnionQueries.cs
@@ -1,5 +1,6 @@
 ﻿using ForeningsPortalen.Application.Features.Unions.Queries;
 using ForeningsPortalen.Application.Features.Unions.Queries.DTOs;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +11,11 @@ namespace ForeningsPortalen.Infrastructure.Queries
 {
     public class UnionQueries : IUnionQueries
     {
+        // private readonly DbContext _dbContext;
+        public UnionQueries(/*DbContext dbContext*/)
+        {
+            //_dbContext = dbContext;
+        }
         UnionQueryResultDto IUnionQueries.GelAllUnions()
         {
             throw new NotImplementedException();
--- ForeningsPortalen.Infrastructure/Repositories/UnionRepository.cs
+++ ForeningsPortalen.Infrastructure/Repositories/UnionRepository.cs
@@ -1,5 +1,6 @@
 ﻿using ForeningsPortalen.Application.Features.Unions.Repositories;
 using ForeningsPortalen.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +11,11 @@ namespace ForeningsPortalen.Infrastructure.Repositories
 {
     public class UnionRepository : IUnionRepository
     {
+        // readonly DbContext _dbContext;
+        public UnionRepository(/*DbContext dbContext*/)
+        {
+            //_dbContext = dbContext;
+        }
         void IUnionRepository.CreateUnion(Union union)
         {
             throw new NotImplementedException();
