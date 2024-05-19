ae28f4a7483bfa14ebc34be20265602120099152
SANNESPC\Sanne <sharrekilde@gmail.com>
2024-05-19 10:02:02 +0200

    UserRole basic logic

--- ForeningsPortalen.Application/Features/Roles/Commands/Implementations/RoleCommands.cs
+++ ForeningsPortalen.Application/Features/Roles/Commands/Implementations/RoleCommands.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ForeningsPortalen.Application.Features.Roles.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
---
+++ ForeningsPortalen.Infrastructure/Repositories/UserRoleRepository.cs
@@ -0,0 +1,19 @@
+﻿using ForeningsPortalen.Application.Features.UserRoles.Repositories;
+using ForeningsPortalen.Infrastructure.Database.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForeningsPortalen.Infrastructure.Repositories
+{
+    public class UserRoleRepository : IUserRoleRepository
+    {
+        private readonly ForeningsPortalenContext _dbCopntext;
+        public UserRoleRepository(ForeningsPortalenContext dbContext)
+        {
+            _dbCopntext = dbContext;
+        }
+    }
+}
