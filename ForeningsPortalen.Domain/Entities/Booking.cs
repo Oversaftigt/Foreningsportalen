696f1f2e0fbf42e9fa5b12d8e1fad0bdf0eca169
Oversaftigt <32249246+Oversaftigt@users.noreply.github.com>
2024-05-23 00:10:07 +0200

    Booking validation part 1

--- ForeningsPortalen.Domain/Entities/Booking.cs
+++ ForeningsPortalen.Domain/Entities/Booking.cs
@@ -36,8 +36,17 @@ namespace ForeningsPortalen.Domain.Entities
         public static Booking CreateBooking(DateTime creationDate, DateTime bookingStart,
             DateTime bookingEnd, List<BookingUnit> bookingUnits, Member user)
         {
+            if(bookingUnits == null) throw new ArgumentNullException(nameof(bookingUnits));
+            if(user == null) throw new ArgumentNullException(nameof(user));
+
+            if (bookingUnits.Count < 1) throw new InvalidOperationException("Booking must have at least one booking unit");
+
             var newBooking = new Booking(creationDate, bookingStart, bookingEnd, bookingUnits, user);
                 return newBooking;
         }
+
+
+
+
     }
 }
