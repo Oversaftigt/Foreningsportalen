9a90466dd1a3f53f84559269858ac29ef46578cc
SANNESPC\Sanne <sharrekilde@gmail.com>
2024-05-23 08:59:42 +0200

    Category queries i infrastructure get all

--- ForeningsPortalen.Infrastructure/Queries/CategoryQueries.cs
+++ ForeningsPortalen.Infrastructure/Queries/CategoryQueries.cs
@@ -1,13 +1,34 @@
 ﻿using ForeningsPortalen.Application.Features.Categories.Queries;
 using ForeningsPortalen.Application.Features.Categories.Queries.DTOs;
+using ForeningsPortalen.Infrastructure.Database.Configuration;
+using Microsoft.EntityFrameworkCore;
 
 namespace ForeningsPortalen.Infrastructure.Queries
 {
     public class CategoryQueries : ICategoryQueries
     {
+        private readonly ForeningsPortalenContext _dbContext;
+        public CategoryQueries(ForeningsPortalenContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
         public IEnumerable<CategoryQueryResultDto> GetCategories()
         {
-            throw new NotImplementedException();
+            var categories = _dbContext.Category.AsNoTracking()
+                .Select(c => new CategoryQueryResultDto()
+                {
+                    Name = c.Name,
+                    DurationType = c.DurationType,
+                    MaxBookingsOfThisCategory = c.MaxBookingsOfThisCategory,
+                });
+
+            if(!categories.Any())
+            {
+                throw new ArgumentNullException("No categories found");
+            }
+
+            return categories;
+
         }
     }
 }
