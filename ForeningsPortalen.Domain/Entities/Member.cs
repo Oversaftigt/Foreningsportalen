d7a9c3bb7eea5260028847be8c06414dd74bf393
Emeli Isabel Egholm Kissov <eiek52989@edu.ucl.dk>
2024-05-30 19:11:33 +0200

    Edit member almost functional functional

--- ForeningsPortalen.Domain/Entities/Member.cs
+++ ForeningsPortalen.Domain/Entities/Member.cs
@@ -1,4 +1,7 @@
-﻿namespace ForeningsPortalen.Domain.Entities
+﻿using System.Data;
+using System.Text.RegularExpressions;
+
+namespace ForeningsPortalen.Domain.Entities
 {
     public class Member : User
     {
@@ -15,27 +18,69 @@
             Address = address;
         }
 
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public DateOnly MoveInDate { get; set; }
+        public string FirstName { get; protected set; }
+        public string LastName { get; protected set; }
+        public DateOnly MoveInDate { get; init; }
         public DateOnly? MoveOutDate { get; set; }
-        public Address Address { get; set; }
-        public IEnumerable<Booking>? Bookings { get; set; }
+        public Address Address { get; init; }
+        public IEnumerable<Booking>? Bookings { get; protected set; }
 
-        public static Member Create(string firstName,
-                                         string lastName,
-                                         DateTime moveInDate,
-                                         Union union,
-                                         Address address,
-                                         string email,
-                                         string phoneNumber
-                                         )
+        public static Member Create(string firstName, string lastName, DateTime moveInDate, Union union,
+                                    Address address, string email, string phoneNumber)
         {
+            if (firstName == null) throw new ArgumentNullException(nameof(firstName));
+            if (lastName == null) throw new ArgumentNullException(nameof(lastName));
+            if (moveInDate != default) throw new ArgumentNullException(nameof(moveInDate));
+            if (union == null) throw new ArgumentNullException(nameof(union));
+            if (address == null) throw new ArgumentNullException(nameof(address));
+            if (email == null) throw new ArgumentNullException(nameof(email));
+            if (!IsPhoneNumberValid(phoneNumber)) throw new InvalidOperationException(nameof(phoneNumber));
 
-            var dateOfMoveIn = DateOnly.FromDateTime( moveInDate );
+            var dateOfMoveIn = DateOnly.FromDateTime(moveInDate);
 
-            var newUnionMember = new Member(firstName, lastName, dateOfMoveIn , address, email, phoneNumber);
+            var newUnionMember = new Member(firstName, lastName, dateOfMoveIn, address, email, phoneNumber);
             return newUnionMember;
         }
+
+        public void Update(string firstName, string lastName, DateTime? moveOutDate, string phoneNumber, Role newRole)
+        {
+            if (firstName == null) throw new ArgumentNullException(nameof(firstName));
+            if (lastName == null) throw new ArgumentNullException(nameof(lastName));
+
+            if (!IsPhoneNumberValid(phoneNumber)) throw new InvalidOperationException(nameof(phoneNumber));
+
+            if (moveOutDate.HasValue)
+            {
+                if (!IsMoveOutDateValid(moveOutDate.Value)) throw new InvalidOperationException(nameof(moveOutDate));
+                MoveOutDate = DateOnly.FromDateTime(moveOutDate.Value);
+            }
+
+            var existingRole = Roles.Last();
+            if (existingRole != newRole)
+            {
+                Roles.Add(newRole);
+            };
+
+            FirstName = firstName;
+            LastName = lastName;
+            PhoneNumber = phoneNumber;
+        }
+
+        private static bool IsPhoneNumberValid(string phoneNumber)
+        {
+            var pattern = @"^\+?\d{ 0,3}[\d\s]{ 7,}$";
+
+            var match = Regex.Match(phoneNumber, pattern);
+            return match.Success;
+        }
+
+        private static bool IsMoveOutDateValid(DateTime moveOutDate)
+        {
+            if (moveOutDate == default) return false;
+            if (moveOutDate < DateTime.MinValue && moveOutDate > DateTime.MaxValue) return false;
+            if (moveOutDate < DateTime.Today) return false;
+            return true;
+        }
+
     }
 }
---
+++ ForeningsPortalen.Infrastructure/Database/UserRoleHistoryConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using ForeningsPortalen.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ForeningsPortalen.Infrastructure.Database
+{
+    //public class UserRoleHistoryConfiguration : IEntityTypeConfiguration<UserRoleHistory>
+    //{
+    //    public void Configure(EntityTypeBuilder<UserRoleHistory> builder)
+    //    {
+
+    //        builder.HasKey(urh => new { urh.UserId, urh.RoleId, urh.FromDate });
+
+    //        builder.HasOne(x => x.User)
+    //               .WithMany(x => x.RoleHistories)
+    //               .HasForeignKey(x => x.UserId);
+
+    //        builder.HasOne(x => x.Role)
+    //               .WithMany(x => x.UserHistories)
+    //               .HasForeignKey(x => x.RoleId);
+
+    //    }
+    //}
+}
